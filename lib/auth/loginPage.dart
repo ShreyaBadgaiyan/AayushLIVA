0b0f9bbef209304ee3f886723bbe31fdc94333cc
Shreya Badgaiyan <117737111+ShreyaBadgaiyan@users.noreply.github.com>
2024-02-01 04:56:52 +0530

    first commit

---
+++ lib/auth/loginPage.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Material(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+                'assets/images/login.png',
+            fit: BoxFit.cover,),
+      
+            SizedBox(
+              height: 20,
+            ),
+      
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
+              child: Column(
+                children: [
+                  Text(
+                    "Welcome",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  SizedBox(
+                    height: 20,
+                  ),
+
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter username',
+                      labelText: 'Username'
+                    ),
+                  ),
+
+                  SizedBox(
+                    height: 20,
+                  ),
+
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Enter password',
+                      labelText: 'Password',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+
+                  ElevatedButton(
+                      onPressed:(){},
+                      child:Text("Login"),
+                  style: TextButton.styleFrom(),)
+                ],
+              ),
+            )
+            
+            
+      
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Center(
+          child: const Text('AYURSCAN',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+           color: Colors.black
+          ),),
+        ),
+      ),
+      body: Container(
+        color: Colors.white,
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
